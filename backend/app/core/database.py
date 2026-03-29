9bdafa066bf69bc4b0b419df89b076d223e6ef41
Mohammad Wasil <88258099+MohWasil@users.noreply.github.com>
2026-03-29 06:47:17 +0430

    Python files cleaning code

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -23,10 +23,6 @@ DEFAULT_DB_URL = f"sqlite+aiosqlite:///{DB_PATH}"
 DATABASE_URL = os.getenv("DATABASE_URL", DEFAULT_DB_URL)
 
 
-
-
-# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./data/purchaseguard.db")
-
 engine = create_async_engine(DATABASE_URL, echo=False)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
